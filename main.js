f3826e4119f7a7dff91354e2858c0c29a06807ce
Erlan <erlan.mirzamatov94@gmail.com>
2022-05-11 13:57:18 +0600

    lesson1

---
+++ main.js
@@ -0,0 +1,80 @@
+// const a = "rrrr";
+// // const - неизменяемая переменная
+// let c = 334;
+// // let - изменяемая переменная
+// console.log(c)
+//
+// c = 23;
+//
+// console.log(c)
+// *********************************//
+
+//                        Тип Данных
+//
+// (string/ number/ boolean/ obj-object/ array)
+
+// const a = "a" //string
+//
+// const b = 20 //number
+//
+// const bool = true; // boolean
+// const bool2 = false;
+//
+// console.log(typeof bool)
+
+// const obj = {
+//     key: "value",
+//     username: "Adam",
+//     data: {
+//         firs_name:"Erick",
+//         last_name: "Elza",
+//     }
+// }
+// console.log(obj.data)
+
+// const arr = [ "Tom", true, false, 34]
+
+// console.log(arr[1])
+
+// const arr2 = [
+//     {
+//         key: "value",
+//     },
+//     {
+//         key: "value",
+//     },
+//     {
+//         key: "value3",
+//     }
+// ]
+// console.log(arr2[2].key)
+//
+// ****************************************
+
+// const name = prompt("Как вас зовут?")
+// console.log("Hello " + name)
+
+// const name = alert("Вам заперещно")
+
+// const first_number = prompt("Add numer");
+// const last_number = prompt("Add  second numer");
+//
+// if (first_number > last_number) {
+//     console.log(first_number + ">" + last_number)
+// } else if (first_number < last_number) {
+//     console.log(first_number + "<" + last_number)
+// } else {
+//     console.log(first_number + "=" + last_number)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
